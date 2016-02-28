6b81ebb6fde4c14bf8391b1ec93acd7f4eaf0f7c
Tim <thung1@binghamton.edu>
2016-02-28 02:40:26 -0800

    Move js to external file

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var scene, camera, renderer;
+init();
+animate();
+
+function init() {
+    scene = new THREE.Scene();
+    var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight;
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.setClearColor(0xffffff, 0);
+    document.body.appendChild(renderer.domElement);
+    camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 10000);
+    camera.position.set(50, 150, 100);
+    scene.add(camera);
+    window.addEventListener('resize', function() {
+        var WIDTH = window.innerWidth,
+            HEIGHT = window.innerHeight;
+        renderer.setSize(WIDTH, HEIGHT);
+        camera.aspect = WIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+    });
+
+    var loader = new THREE.ColladaLoader();
+    loader.options.convertUpAxis = true;
+    loader.load('models/gen/example.dae', function(collada) {
+        var dae = collada.scene;
+        var skin = collada.skins[0];
+        dae.position.set(0, 0, 0); //x,z,y- if you think in blender dimensions ;)
+        dae.scale.set(1.5, 1.5, 1.5);
+        scene.add(dae);
+    });
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    controls.update();
+}
